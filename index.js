ee7aedf31f2e33cc6175a75a373931b79a8f08bc
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2024-01-15 21:02:02 -0600

    added func for searching stock names

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 // may want to use express session to save session data
 // find out how to import tailwind and postcss
 import bodyParser from "body-parser";
 import ejs from "ejs";
 
-import {getStockData} from "./src/middleware/getStockData.js";
-import {calculateIntrinsicValue} from "./src/services/calculateIntrinsicValue.js";
-
-
+import { getStockData } from "./src/middleware/getStockData.js";
+import { calculateIntrinsicValue } from "./src/services/calculateIntrinsicValue.js";
+import { getStockName } from "./src/middleware/getStockName.js";
 const app = express();
 
 app.use(express.static("views"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("home");
-})
+});
 
 app.get("/getIntrinsicValue", (req, res) => {
-  
   const promisedData = Promise.resolve(getStockData());
 
   promisedData.then((value) => {
@@ -29,34 +27,49 @@ app.get("/getIntrinsicValue", (req, res) => {
     const cashPerShare = value.totalCashPerShare.raw;
     const stockPrice = value.currentPrice.raw;
 
-    console.log(value.earningsGrowth.raw)
-    console.log(value.totalCashPerShare.raw)
-    console.log(value.currentPrice.raw)
+    console.log(value.earningsGrowth.raw);
+    console.log(value.totalCashPerShare.raw);
+    console.log(value.currentPrice.raw);
     try {
-    
-      const intrinsicValue = (
-        cashPerShare * (1 + earningsGrowth)
-        * (stockPrice / cashPerShare)
-      )
-  
-      console.log("result: " + intrinsicValue);
-      
+      const intrinsicValue =
+        cashPerShare * (1 + earningsGrowth) * (stockPrice / cashPerShare);
+
+      res.render("intrinsicValue", { calculatedValue: intrinsicValue });
     } catch (error) {
       console.error(error);
     }
+  });
+});
 
+app.get("/getStockName", (req, res) => {
+  const stockName = req.body.stockName;
+ 
+
+  let searchedStock = {
+    longName: "",
+    quoteType: "",
+    symbol: "",
+    exchange: "",
+  };
+
+  try {
+    searchedStock = getStockName();
+    console.log(searchedStock.longName)
+    res.render("stockName", {stock: searchedStock})
+    
+  } catch (error) {
+    console.error(error);
+  }
+  
 
   
-  })
-  
-  res.render("intrinsicValue");
-})
+});
 
 let port = process.env.PORT || 3000;
 if (port == null || port == "") {
   port = `0.0.0.0:$PORT`;
 }
 
-app.listen(port, "0.0.0.0",function() {
+app.listen(port, "0.0.0.0", function () {
   console.log("Server has started successfully ");
 });
